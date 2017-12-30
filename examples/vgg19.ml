5f065fa17e3f585091d86843e7836970545893fc
laurent <laurent.mazare@gmail.com>
2017-12-30 21:26:45 +0000

    Handle non-square images.

--- examples/vgg19.ml
+++ examples/vgg19.ml
@@ -31,12 +31,6 @@ let conv2d th ~in_channels ~out_channels ~name =
 let max_pool th =
   Ops.maxPool th ~ksize:[1; 2; 2; 1] ~strides:[1; 2; 2; 1] ~padding:"SAME" ~data_format:"NHWC"
 
-let read_image filename =
-  Tensor_handle.of_string_exn filename
-  |> Ops.readFile
-  |> Ops.decodePng ~type_dtype:Type.UInt8 ~channels:3
-  |> Ops.cast ~type_dstT:Type.Float
-
 let scalar_int32 int =
   let tensor = Tensor.create Int32 [||] in
   Tensor.copy_elt_list tensor (List.map [int] ~f:Int32.of_int_exn);
@@ -47,7 +41,32 @@ let const_int32 int_list =
   Tensor.copy_elt_list tensor (List.map int_list ~f:Int32.of_int_exn);
   Tensor_handle.create_exn (Tensor.P tensor)
 
+let const_float32 float_list =
+  let tensor = Tensor.create Float32 [| List.length float_list |] in
+  Tensor.copy_elt_list tensor float_list;
+  Tensor_handle.create_exn (Tensor.P tensor)
+
 let reshape th ~shape = Ops.reshape th (const_int32 shape)
+
+let read_image filename =
+  let image =
+    Tensor_handle.of_string_exn filename
+    |> Ops.readFile
+    |> Ops.decodePng ~type_dtype:Type.UInt8 ~channels:3
+  in
+  let height, width =
+    match Tensor_handle.dims image with
+    | [ height; width; 3 ] -> height, width
+    | _ -> assert false
+  in
+  let min_edge = Int.min height width in
+  Ops.slice image
+    (const_int32 [ (height - min_edge)/2; (width - min_edge)/2; 0 ])
+    (const_int32 [ min_edge; min_edge; 3 ])
+  |> reshape ~shape:[1; min_edge; min_edge; 3]
+  |> fun th -> Ops.resizeBicubic th (const_int32 [ img_dim; img_dim ]) ~align_corners:false
+  |> fun th -> Ops.reverseV2 th (const_int32 [-1]) (* Switch from RGB to BGR. *)
+  |> fun th -> Ops.(-) th (const_float32 [ 103.939; 116.779; 123.68 ])
 
 let dense ~name out_dims th =
   let in_dims = Tensor_handle.dims th |> List.last_exn in
@@ -68,7 +87,6 @@ let vgg19 filename =
     |> max_pool
   in
   read_image filename
-  |> reshape ~shape:[1; img_dim; img_dim; 3]
   |> block 2 ~block_idx:1 ~out_channels:64
   |> block 2 ~block_idx:2 ~out_channels:128
   |> block 4 ~block_idx:3 ~out_channels:256
