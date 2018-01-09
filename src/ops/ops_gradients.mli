2e124db8f7111a656cced11530ef4807cc5c8839
laurent <laurent.mazare@gmail.com>
2018-01-09 07:23:32 +0000

    Do not expose multi gradient registration.

--- src/ops/ops_gradients.mli
+++ src/ops/ops_gradients.mli
@@ -13,18 +13,6 @@ val add
   -> t
   -> unit
 
-type multi =
-  { g : 'a .
-          (  self:([< `float | `double] as 'a) T.t
-          -> gradients:'a T.t Map.M(Int).t
-          -> T.p option list)
-  }
-
-val add_multi
-  :  Op.Name.t
-  -> multi
-  -> unit
-
 val find : Op.Name.t -> (self:T.p -> gradient:T.p -> T.p option list) option
 val find_multi
   :  Op.Name.t
